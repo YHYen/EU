0c593f44707f66708bb3b61244e561b866f86c4b
YHYen <cloudyhyen@gmail.com>
2023-04-02 17:50:16 +0800

    Using json file to save and load data

--- EU/ViewController.swift
+++ EU/ViewController.swift
@@ -13,92 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var editBarButton: UIBarButtonItem!
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
-
-    var members = ["Austria",
-                  "Belgium",
-                  "Bulgaria",
-                  "Croatia",
-                  "Cyprus",
-                  "Czechia",
-                  "Denmark",
-                  "Estonia",
-                  "Finland",
-                  "France",
-                  "Germany",
-                  "Greece",
-                  "Hungary",
-                  "Ireland",
-                  "Italy",
-                  "Latvia",
-                  "Lithuania",
-                  "Luxembourg",
-                  "Malta",
-                  "Netherlands",
-                  "Poland",
-                  "Portugal",
-                  "Romania",
-                  "Slovakia",
-                  "Slovenia",
-                  "Spain",
-                  "Sweden",
-                  "United Kingdom"]
-    var capitals = ["Vienna",
-                    "Brussels",
-                    "Sofia",
-                    "Zagreb",
-                    "Nicosia",
-                    "Prague",
-                    "Copenhagen",
-                    "Tallinn",
-                    "Helsinki",
-                    "Paris",
-                    "Berlin",
-                    "Athens",
-                    "Budapest",
-                    "Dublin",
-                    "Rome",
-                    "Riga",
-                    "Vilnius",
-                    "Luxembourg (city)",
-                    "Valetta",
-                    "Amsterdam",
-                    "Warsaw",
-                    "Lisbon",
-                    "Bucharest",
-                    "Bratislava",
-                    "Ljubljana",
-                    "Madrid",
-                    "Stockholm",
-                    "London"]
-    var usesEuro = [true,
-                    true,
-                    false,
-                    false,
-                    true,
-                    false,
-                    false,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    false,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    false,
-                    true,
-                    false,
-                    true,
-                    true,
-                    true,
-                    false,
-                    false]
-    
     var nations: [Nation] = []
     
     override func viewDidLoad() {
@@ -107,10 +21,7 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        for index in 0..<members.count {
-            let newNation = Nation(country: members[index], capital: capitals[index], ueseEuro: usesEuro[index])
-            nations.append(newNation)
-        }
+        loadData()
     }
     
     
@@ -130,6 +41,37 @@ class ViewController: UIViewController {
                 // cancel the selection state of the cell
                 tableView.deselectRow(at: selectedIndexPath, animated: true)
             }
+        }
+    }
+    
+    
+    func loadData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("nations").appendingPathExtension("json")
+        
+        guard let data = try? Data(contentsOf: documentURL) else {return}
+        let jsonDecoder = JSONDecoder()
+        
+        do {
+            nations = try jsonDecoder.decode(Array<Nation>.self, from: data)
+            tableView.reloadData()
+        } catch {
+            print("😡 ERROR: Could not load data! \(error.localizedDescription).")
+        }
+    }
+    
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("nations").appendingPathExtension("json")
+        
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(nations)
+        
+        do {
+            try data?.write(to: documentURL, options: .noFileProtection)
+        } catch {
+            print("😡 ERROR: Could not save data! \(error.localizedDescription).")
         }
     }
     
@@ -160,6 +102,8 @@ class ViewController: UIViewController {
             // scroll the tableView to new data
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
+        
+        saveData()
     }
 
 
@@ -195,6 +139,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         if editingStyle == .delete {
             nations.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            saveData()
         }
     }
     
@@ -202,6 +147,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let itemToMove = nations[sourceIndexPath.row]
         nations.remove(at: sourceIndexPath.row)
         nations.insert(itemToMove, at: destinationIndexPath.row)
+        saveData()
     }
 }
 
